848949c6b1c2c30401da5c697162c2c46d232e6c
Ketut Kusuma <71806737+KetutKusuma@users.noreply.github.com>
2024-01-27 15:47:53 +0800

    masuk ke dalam screen

--- lib/src/on_boarding/persentation/views/on_boarding_screen.dart
+++ lib/src/on_boarding/persentation/views/on_boarding_screen.dart
@@ -1,4 +1,6 @@
+import 'package:education_bloc_app/src/on_boarding/persentation/cubit/on_boarding_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -7,6 +9,13 @@ class OnBoardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    // context.read
+    return BlocListener<OnBoardingCubit, OnBoardingState>(
+      listener: (context, state) {
+        if (state is OnBoardingStatus) {
+          Navigator.pushReplacementNamed(context, '/home');
+        }
+      },
+    );
   }
 }

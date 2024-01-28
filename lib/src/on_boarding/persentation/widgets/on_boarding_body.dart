6efbde4c0e3ad5a077959bf13d44c4d9ec933b85
Ketut Kusuma <71806737+KetutKusuma@users.noreply.github.com>
2024-01-28 18:38:11 +0800

    buat background

---
+++ lib/src/on_boarding/persentation/widgets/on_boarding_body.dart
@@ -0,0 +1,16 @@
+import 'package:education_bloc_app/src/on_boarding/domain/page_content.dart';
+import 'package:flutter/material.dart';
+
+class OnBoardingBody extends StatelessWidget {
+  const OnBoardingBody({
+    required this.pageContent,
+    super.key,
+  });
+
+  final PageContent pageContent;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
